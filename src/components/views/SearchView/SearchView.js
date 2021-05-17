f2dcf0c972bc4ec08dcfa7a988910ae2f59e6461
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2021-05-17 13:26:59 +0200

    add: search as you type

--- src/components/views/SearchView/SearchView.js
+++ src/components/views/SearchView/SearchView.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchView = () => {
-    return ( 
-        <h1>Hello</h1>
-     );
+const SearchView = ({ searchQuery, setSearchQuery }) => {
+    return (
+        <div >
+        
+            <input
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                type="text"
+                id="header-search"
+                placeholder="Search blog posts"
+                name="s"
+            />
+        </div>
+    );
 }
- 
+
+SearchView.propTypes = {
+    searchQuery: PropTypes.string,
+    setSearchQuery: PropTypes.func,
+}
 export default SearchView;
--- src/components/views/TodoTasksView/TodoTasksView.js
+++ src/components/views/TodoTasksView/TodoTasksView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import { todoState, todoListFilterState } from '../../../App';
 import { useRecoilValue, atom, selector, useRecoilState } from 'recoil';
@@ -6,39 +6,56 @@ import AddTaskView from '../AddTaskView/AddTaskView';
 import TaskView from '../TaskView/TaskView';
 import FilterTasks from '../FilterTasks/FilterTasks';
 import StatsView from '../StatsView/StatsView';
+import SearchView from '../SearchView/SearchView';
 
 
-  const filteredTodoListState = selector({
-    key: 'filteredTodoListState',
-    get: ({get}) => {
-      const filter = get(todoListFilterState);
-      const list = get(todoState);
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoState);
+
+    switch (filter) {
+      case 'Show All':
+        return list.filter((item) => item);
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.completed);
+      default:
+        return list;
+    }
+  },
+});
+
+const filterPosts = (posts, query) => {
+  if (!query) {
+      return posts;
+  }
   
-      switch (filter) {
-        case 'Show All':
-          return list.filter((item) => item);
-        case 'Show Uncompleted':
-          return list.filter((item) => !item.completed);
-        default:
-          return list;
-      }
-    },
+  return posts.filter((post) => {
+      const postName = post.title.toLowerCase();
+      return postName.includes(query);
   });
-
-  
+};
 
 const TodoTasksView = () => {
-    const tasks = useRecoilValue(filteredTodoListState)
-    return (
-        <>
-            <Link to='/task'>Task</Link>
-            <AddTaskView />
-            <StatsView />
-            <FilterTasks filteredTodoListState={filteredTodoListState} />
-            {tasks.map((task) =>
-                <TaskView key={task.id} item={task} />)}
-        </>
-    );
+  const tasks = useRecoilValue(filteredTodoListState)
+  const [searchQuery, setSearchQuery] = useState('');
+  const filteredPosts = filterPosts(tasks, searchQuery);
+
+  return (
+    <>
+      <Link to='/task'>Task</Link>
+      <SearchView 
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+      />
+      <AddTaskView />
+      <StatsView />
+      <FilterTasks filteredTodoListState={filteredTodoListState} />
+      {filteredPosts.map((task) =>
+        <TaskView key={task.id} item={task} />)}
+    </>
+  );
 }
 
 
