44f652757809bb89accb711d4f4d1e28bfee7dca
kamil.mrowka <kamil.mrowka@outlook.com.au>
2021-05-19 13:45:14 +0200

    add decoration

--- src/components/views/StatsView/StatsView.js
+++ src/components/views/StatsView/StatsView.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { todoState } from '../../../App';
-import {selector, useRecoilValue} from 'recoil';
+import { selector, useRecoilValue } from 'recoil';
+import { Box, Container, Flex } from '@theme-ui/components';
 
 const todoListStatsState = selector({
-    key: 'todoListStatsState',
-    get: ({get}) => {
-      const todoList = get(todoState);
-      const totalNum = todoList.length;
-      const totalCompletedNum = todoList.filter((item) => item.completed).length;
-      const totalUncompletedNum = totalNum - totalCompletedNum;
-  
-      return {
+  key: 'todoListStatsState',
+  get: ({ get }) => {
+    const todoList = get(todoState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.completed).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
 
-        totalCompletedNum,
-        totalUncompletedNum,
-      };
-    },
-  });
+    return {
 
-  const StatsView = () => {
-    const {
-      totalNum,
       totalCompletedNum,
       totalUncompletedNum,
-      percentCompleted,
-    } = useRecoilValue(todoListStatsState);
-    
-    return (
-      <ul>
-        <li>Items completed: {totalCompletedNum}</li>
-        <li>Items not completed: {totalUncompletedNum}</li>
-      </ul>
-    );
-  }
+    };
+  },
+});
 
-  export default StatsView;
+const StatsView = () => {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  return (
+    <Container>
+      <Flex sx={{ fontSize: 12,}}>
+        <Box p={2} >Done: ({totalCompletedNum}) </Box>
+        <Box p={2}>Uncompleted: ({totalUncompletedNum})</Box>
+      </Flex>
+    </Container>
+  );
+}
+
+export default StatsView;
